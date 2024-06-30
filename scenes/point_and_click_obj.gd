30ebadb009a7c27ce1ab509777c38889bd394d6c
Yuri <yuriandradeog@gmail.com>
2024-06-30 10:11:55 -0300

    Revert "erro"
    This reverts commit aed54cec04706b550e7ac57b6220b1f798d3b953.

--- scenes/point_and_click_obj.gd
+++ scenes/point_and_click_obj.gd
@@ -6,21 +6,16 @@ var defaultColor := Color(255 / 255, 255 / 255, 255 / 255, 255 / 255)
 var hoverColor := Color(0.8, 0, 0, 220)
 var mouseInArea := false
 
-
 func _ready():
 	areaDisplay.modulate = defaultColor
 
-
 func _on_area_2d_mouse_entered():
+	print ("entrou")
 	areaDisplay.modulate = hoverColor
 	mouseInArea = true
 
 
 func _on_area_2d_mouse_exited():
+	print ("saiu")
 	areaDisplay.modulate = defaultColor
 	mouseInArea = false
-
-
-func _on_area_2d_input_event(viewport, event, shape_idx):
-	if event.is_action_pressed("mouse_left") and mouseInArea:
-		get_tree().change_scene_to_packed(nextScene)
