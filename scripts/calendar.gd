a667143bc9dfc927151ce1c82bb83a5f27ed9188
Yuri <yuriandradeog@gmail.com>
2024-09-23 21:19:25 -0300

    inicio do sistema de calendario

---
+++ scripts/calendar.gd
@@ -0,0 +1,63 @@
+extends Node2D
+
+@export var selected_day : Sprite2D
+@export var day_label : Label
+
+var scene_to_instance = preload("res://objects/trabalho_verde.tscn")
+
+var time = 0
+var current_day = 1
+var continue_time = true
+ 
+func instanceObject():
+	var object = scene_to_instance.instance()
+	call_deferred("add_child",object)
+
+func printDay(day_num):
+	match(day_num):
+		0:
+			day_label.text = "Dia: Domingo"
+		1:
+			day_label.text = "Dia: Segunda"
+		2:
+			day_label.text = "Dia: Terça"
+		3:
+			day_label.text = "Dia: Quarta"
+		4:
+			day_label.text = "Dia: Quinta"
+		5:
+			day_label.text = "Dia: Sexta"
+		6:
+			day_label.text = "Dia: Sábado"
+
+func _ready():
+	var menu_button_trabalhos = $MenuButtonTrabalhos
+	var popup = menu_button_trabalhos.get_popup()
+	popup.id_pressed.connect(menuTrabalhos)
+
+func menuTrabalhos(id):
+	match(id):
+		0:
+			print("verde")
+			instanceObject()
+		1:
+			print("azul")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	printDay(current_day)
+	
+	if continue_time:
+		time += delta
+	if time > 1:
+		current_day+= 1;
+		selected_day.offset += Vector2(50,0)
+		if selected_day.offset.x >= 350:
+			selected_day.offset += Vector2(-350,50)
+		if current_day == 7:
+			current_day = 0
+			continue_time = false
+		time = 0
+
+func _on_continue_button_pressed():
+	continue_time = true
